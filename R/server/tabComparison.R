0aeafd19371ee614391afde54a8483f7c02e2259
Aimeric Bruno <abruno@ad-cng.cng.fr>
2019-07-18 15:28:20 +0000

    Add new file

---
+++ R/server/tabComparison.R
@@ -0,0 +1,48 @@
+dataSet <- reactive ({
+	files <- paste(reportingFolder,"/",input$testRunList,"_globalstat.txt",sep="")
+	data<-data.frame()
+
+	for(f in files) {
+		data = rbind(data,readCsvSpace2(f))
+	}
+	return(data)
+})
+
+output$testMulti <- renderPlotly({
+	if(nrow(dataSet())) {
+		ggplotly(
+			ggplot( dataSet(),
+				aes( 	x = get("DURATION(mn)"),
+					y = get("YIELD(b)"),
+					col = FLOWCELL,
+					group=1,
+					text = paste(	FLOWCELL,
+							'<br>DURATION (mn): ',get("DURATION(mn)"),
+							'<br>YIELD: ',format(get("YIELD(b)"),big.mark=' '),
+							sep=""
+					)
+				)
+			) +
+			geom_line() +
+			theme_bw(),
+			
+			dynamicTicks=T,
+			tooltip = "text"
+		)  %>% plotlyConfig()
+	}
+})
+
+# update drop-down list of run
+observe({
+	listRun = runInfoStatReader()$FLOWCELL
+	if(is.null(listRun)) {
+		listRun <- character(0)
+	}
+	
+	updateSelectizeInput( 
+		session,
+		"testRunList",
+		choice = listRun,
+		server=TRUE
+	)
+})
