fa9cd23d2b2b3f74baab3e81eafa1180c360e6c9
Aimeric Bruno <abruno@ad-cng.cng.fr>
2019-07-18 15:23:58 +0000

    Add new file

---
+++ R/app.R
@@ -0,0 +1,19 @@
+library(ggplot2)
+library(plotly)
+library(shiny)
+library(shinydashboard)
+library(data.table)
+library(readr)
+#library(DT)
+
+require(bit64)
+
+## FRONTEND
+source("ui/ui.R")
+
+## BACKEND
+source("server/server.R")
+
+shinyApp(ui, server,options=list(port=80,host="172.25.123.149"))
+#shinyApp(ui, server,options=list(port=3140,host="195.83.222.110"))	# etna48
+#shinyApp(ui, server,options=list(port=3140,host="195.83.222.111"))	# etna49
