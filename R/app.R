6e74c7979f83534972b84b6454d6ea6970017390
Aimeric Bruno <abruno@ad-cng.cng.fr>
2019-11-20 10:55:49 +0000

    change port

--- R/app.R
+++ R/app.R
@@ -17,7 +17,7 @@ source("ui/ui.R")
 ## BACKEND
 source("server/server.R")
 
-shinyApp(ui, server,options=list(port=5115,host="172.17.0.2"))
+shinyApp(ui, server,options=list(port=80,host="172.17.0.2"))
 #shinyApp(ui, server,options=list(port=80,host="172.25.123.59"))
 #shinyApp(ui, server,options=list(port=80,host="172.25.123.149"))
 #shinyApp(ui, server,options=list(port=3140,host="195.83.222.110"))	# etna48
