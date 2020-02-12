c01b1b87b90bd81ab1b960ac25458902b5cc273f
abruno <abruno@rdbioseq4.dev.os.cnrgh.fr>
2020-02-12 17:13:06 +0100

    add main dockerfile containing both the app and the preprocess

--- demo/demo.sh
+++ demo/demo.sh
@@ -2,23 +2,8 @@
 
 # execute the script generating data for preprocess program
 echo "STARTING file generation"
-/usr/home/root/demo/boardionDemo_generateFile.pl /usr/home/root/demo/data /usr/home/root/demo/stat 20 50000 /usr/home/root/demo/raw/* > /usr/home/root/demo/logs/seq_sum_generation.log 2>&1 &
+/usr/local/src/demo/boardionDemo_generateFile.pl /usr/local/src/demo/data /usr/local/src/demo/stat 20 50000 /usr/local/src/demo/raw/* > /usr/local/src/demo/logs/seq_sum_generation.log 2>&1 &
 sleep 4
 
-# generate stat file before launching app
-echo "FIRST PREPROCESS"
-/usr/home/root/demo/boardion_preprocess -i /usr/home/root/demo/data -o /usr/home/root/demo/stat/ > /usr/home/root/demo/logs/preprocess.log 2>&1
-
-# running every 5 minutes the preprocess program
-echo "SETUP crontab"
-echo "*/2 * * * * /usr/home/root/demo/boardion_preprocess -i /usr/home/root/demo/data -o /usr/home/root/demo/stat/ >> /usr/home/root/demo/logs/preprocess.log 2>&1" > /usr/home/root/demo/demo-crontab2
-crontab /usr/home/root/demo/demo-crontab2
-
-# start cron daemon
-echo "STARTING cron"
-crond
-
-# execute the application
-echo "STARTING app"
-cd /usr/home/root/app/
-Rscript boardion_app.R 80 /usr/home/root/demo/stat
+# start preprocess and webapp
+/usr/local/src/docker_entrypoint.sh 600 2 80
---
+++ docker_entrypoint.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+usage() {
+
+	echo "
+Usage: $0 [-h] [-d <DURATION>] [-f <FREQUENCIE>] [-p <PORT>]
+
+-d <INT> duration of a step in secondes (10 minutes == 400 values per graph for run of 4000 minutes) [600]
+-f <INT> frequencie of the cron genrating stat file in minute (every <INT> minutes), it's the refresh rate of the data in the app [5]
+-p <INT> port to listen [80]
+" 1>&2
+
+	exit 1
+}
+
+DURATION=600
+FREQUENCIE=5
+PORT=80
+
+while getopts "hd:f:p:" opts; do
+	case ${opts} in
+		d) DURATION=${OPTARG} ;;
+		f) FREQUENCIE=${OPTARG} ;;
+		p) PORT=${OPTARG} ;;
+		h) usage;;
+		:) usage;;
+		?) usage;;
+	esac
+done
+
+shift $((OPTIND-1))
+
+# generate stat file before launching the app, because the app crash if input folder empty
+echo "generating intial stat files..."
+/usr/local/src/boardion_preprocess -d ${DURATION} -i /usr/local/src/data -o /usr/local/src/stat/ > /usr/local/src/logs/preprocess.log 2>&1
+
+# running every X minutes the preprocess program
+echo "setup crontab..."
+echo "*/${FREQUENCIE} * * * * /usr/local/src/boardion_preprocess -d ${DURATION} -i /usr/local/src/data -o /usr/local/src/stat/ >> /usr/local/src/logs/preprocess.log 2>&1" > /usr/local/src/boardion-crontab
+crontab /usr/local/src/boardion-crontab
+
+# start cron daemon
+echo "start cron..."
+crond
+
+# execute the application
+echo "start app..."
+Rscript /usr/local/src/app/boardion_app.R ${PORT} /usr/local/src/stat
