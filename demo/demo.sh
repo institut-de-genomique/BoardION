40197c7e615cf35abca07fd49627905724325c39
abruno <abruno@rdbioseq4.dev.os.cnrgh.fr>
2020-02-13 16:49:04 +0100

    correct demo path and demo cron frequencie

--- demo/demo.sh
+++ demo/demo.sh
@@ -2,8 +2,8 @@
 
 # execute the script generating data for preprocess program
 echo "STARTING file generation"
-/usr/local/src/demo/boardionDemo_generateFile.pl /usr/local/src/demo/data /usr/local/src/demo/stat 20 50000 /usr/local/src/demo/raw/* > /usr/local/src/demo/logs/seq_sum_generation.log 2>&1 &
+/usr/local/src/demo/boardionDemo_generateFile.pl /usr/local/src/data /usr/local/src/stat 20 50000 /usr/local/src/raw/* > /usr/local/src/logs/seq_sum_generation.log 2>&1 &
 sleep 4
 
 # start preprocess and webapp
-/usr/local/src/docker_entrypoint.sh 600 2 80
+/usr/local/src/docker_entrypoint.sh -d 600 -f 2 -p 80
