33d00f21a120977952e2169028f27f6c7f54aa3d
Aimeric Bruno <aimeric.bruno@genoscope.cns.fr>
2020-03-09 17:10:33 +0000

    Update demo/demo.sh

--- demo/demo.sh
+++ demo/demo.sh
@@ -2,8 +2,10 @@
 
 # execute the script generating data for preprocess program
 echo "starting file generation"
-/usr/local/src/demo/boardionDemo_generateFile.pl /usr/local/src/data /usr/local/src/stat 20 50000 /usr/local/src/raw/* > /usr/local/src/logs/seq_sum_generation.log 2>&1 &
+/usr/local/src/demo/boardionDemo_generateFile.pl /usr/local/src/data /usr/local/src/stat 20 50000 /usr/local/src/raw/*sequencing_summary.txt > /usr/local/src/logs/seq_sum_generation.log 2>&1 &
 sleep 4
+
+mkdir /usr/local/src/stat
 
 # start preprocess and webapp
 /usr/local/src/docker_entrypoint.sh -d 600 -f 2 -p 80
