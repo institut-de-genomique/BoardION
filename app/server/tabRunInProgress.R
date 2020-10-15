2f6c3e56ba303fca20dde3226b96888969aa1fcb
Aimeric Bruno <aimeric.bruno@genoscope.cns.fr>
2020-10-15 09:46:41 +0000

    debug b/mn -> b/s

--- app/server/tabRunInProgress.R
+++ app/server/tabRunInProgress.R
@@ -121,7 +121,7 @@ observeEvent( ripList(), {
 
 				# for speed we display the value of the last non cumulative step
 				output[[valBoxSpeed]] <- renderValueBox({ 
-					valueBox( paste( formatNumber( tail( rip_currentFileReader[[ fc ]](), 1)$`Speed(b/mn)` ), "b/s"), "Speed")
+					valueBox( paste( formatNumber( tail( rip_currentFileReader[[ fc ]](), 1)$`Speed(b/s)` ), "b/s"), "Speed")
 				})
 
 				output[[valBoxQuality]] <- renderValueBox({
